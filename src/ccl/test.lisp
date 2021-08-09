8e95b2fa75c178265dcccc17c98e723fd41228f9
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-09 14:36:57 +0300

    9 авг 2021 г. 14:36:56 make-tangent-belts make-radial-belts

--- src/ccl/test.lisp
+++ src/ccl/test.lisp
@@ -3,6 +3,7 @@
 (in-package #:mnas-icem/ccl)
 
 (require :mnas-string)
+(require :math/coord)
 
 (defun number-to-string (val &key (fmt "~,2@F"))
   "@b(Описание:) функция @b(number-to-string) возвращает строку,
@@ -23,7 +24,7 @@
   "@b(Описание:) функция @b(mm->m) переводит милиметры в метры."
   (/ value 1000.0))
 
-(defun make-tangent-belts (number x y r-min r-max theta-min theta-max)
+(defun make-tangent-belts (number x y r-min r-max theta-min theta-max alpha)
   "@b(Описание:) функция @b(make-tangent-belts) создает поверхности,
  представляющие из себя окружные пояса.
 
@@ -35,20 +36,23 @@
 @item(r-max - радиус минимальный;) 
 @item(r-min - радиус максимальный;)
 @item(theta-min - угол минимальный;)
-@item(theta-max - угол максимальный.)  
+@item(theta-max - угол максимальный;)
+@item(alpha - угол поворота поясов вокруг оси X, градусы.)
+alfa
 @end(list)
 
  @b(Пример использования:)
 @begin[lang=lisp](code)
- (make-tangent-belts 50 466.5 0.0 411.0 477.0 -11.25 11.25)
- (make-tangent-belts 50 466.5 0.0 411.0 477.0  11.25 33.75)
+(progn
+  (make-tangent-belts 10 450 530 10 110 -180.0 180. 0.0)
+  (make-tangent-belts 10 450 530 10 110 -180.0 180. 22.5))
 @end(code)
 "
   (loop :for i :from 0 :below number :do
     (let ((r-i    (+ r-min (* (/ i      number) (- r-max r-min))))
           (r-i+1  (+ r-min (* (/ (1+ i) number) (- r-max r-min)))))
       (format t
-              "LINE: Line ~A ~A ~A ~A
+              "LINE: Line ~A ~A ~A ~A ~A 
   Apply Instancing Transform = On
   Colour = 1, ~A, 0
   Colour Map = Default Colour Map
@@ -64,8 +68,8 @@
   Max = 0.0 [Pa]
   Min = 0.0 [Pa]
   Option = Two Points
-  Point 1 = ~A [m], ~A [m], 0 [m]
-  Point 2 = ~A [m], ~A [m], 0 [m]
+  Point 1 = ~F [m], ~F [m], ~F [m]
+  Point 2 = ~F [m], ~F [m], ~F [m]
   Range = Global
   Visibility = On
   OBJECT VIEW TRANSFORM: 
@@ -87,13 +91,21 @@
   END
 END
 "
-              (number-to-string x) (number-to-string y) (number-to-string r-i) (number-to-string r-i+1)
-              (nth-value 1 (floor (1+ i) 2))
-              (mm->m x) (mm->m r-i  )
-              (mm->m x) (mm->m r-i+1))
+              (number-to-string alpha)
+              (number-to-string x)
+              (number-to-string y)
+              (number-to-string r-i)
+              (number-to-string r-i+1)       ;; Header
+              (nth-value 1 (floor (1+ i) 2)) ;; Color
+              (mm->m x)                      ;; x Point 1
+              (* (cos (math/coord:dtr alpha)) (mm->m (+ y r-i))) ;; y Point 1
+              (* (sin (math/coord:dtr alpha)) (mm->m (+ y r-i))) ;; z Point 1
+              (mm->m x) ;; x Point 2
+              (* (cos (math/coord:dtr alpha)) (mm->m (+ y r-i+1))) ;; y Point 2
+              (* (sin (math/coord:dtr alpha)) (mm->m (+ y r-i+1)))) ;; z Point 2
       (format t
               "
-SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
+SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A ~A
   Apply Instancing Transform = On
   Apply Texture = Off
   Blend Texture = On
@@ -114,7 +126,7 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   Line Colour = 0, 0, 0
   Line Colour Mode = Default
   Line Width = 1
-  Location List = /LINE:Line ~A ~A ~A ~A
+  Location List = /LINE:Line ~A ~A ~A ~A ~A
   Max = 0.0 [Pa]
   Meridional Point 1 = 0 [m], 1 [m]
   Meridional Point 2 = 1 [m], 2 [m]
@@ -125,9 +137,9 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   Project to AR Plane = On
   Range = Global
   Render Edge Angle = 0 [degree]
-  Rotation Axis From = 0 [m], 0 [m], 0 [m]
-  Rotation Axis To = 1 [m], 0 [m], 0 [m]
-  Rotation Axis Type = Principal Axis
+  Rotation Axis From = 0 [m], ~F [m], ~F [m]
+  Rotation Axis To = 1 [m], ~F [m], ~F [m]
+  Rotation Axis Type = Rotation Axis
   Specular Lighting = On
   Starting Axial Shift = 0.0 [m]
   Starting Radial Shift = 0.0 [m]
@@ -141,7 +153,7 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   Texture Type = Predefined
   Theta Max = ~A [degree]
   Theta Min = ~A [degree]
-  Theta Points = 10
+  Theta Points = 50
   Tile Texture = Off
   Transform Texture = Off
   Transparency = 0.0
@@ -166,14 +178,31 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   END
 END
 "
-              (number-to-string x) (number-to-string r-i) (number-to-string r-i+1) (number-to-string theta-min) (number-to-string theta-max)
-              (nth-value 1 (floor (1+ i) 2)) ;; color
-              (number-to-string x) (number-to-string y) (number-to-string r-i) (number-to-string r-i+1) ;; Location
-              theta-max theta-min))))
+              (number-to-string alpha)
+              (number-to-string x)
+              (number-to-string r-i)
+              (number-to-string r-i+1)
+              (number-to-string theta-min)
+              (number-to-string theta-max) ;; SURFACE OF REVOLUTION:
+              
+              (nth-value 1 (floor (1+ i) 2)) ;; Colour
+
+              (number-to-string alpha)
+              (number-to-string x)
+              (number-to-string y)
+              (number-to-string r-i)
+              (number-to-string r-i+1) ;; Location
+              
+              (* (cos (math/coord:dtr alpha)) (mm->m y)) ;; y Rotation Axis From 
+              (* (sin (math/coord:dtr alpha)) (mm->m y)) ;; z Rotation Axis From
+              (* (cos (math/coord:dtr alpha)) (mm->m y)) ;; y Rotation Axis To
+              (* (sin (math/coord:dtr alpha)) (mm->m y)) ;; z Rotation Axis From
+              theta-max
+              theta-min))))
 
 
 
-(defun make-radial-belts (number x y r-min r-max theta-min theta-max)
+(defun make-radial-belts (number x y r-min r-max theta-min theta-max alpha)
   "@b(Описание:) функция @b(make-radial-belts) создает поверхности,
  представляющие из себя радиальные пояса.
 
@@ -185,10 +214,11 @@ END
  @item(r-max - радиус минимальный;)
  @item(r-min - радиус максимальный;)
  @item(theta-min - угол минимальный;)
- @item(theta-max - угол максимальный.)
+ @item(theta-max - угол максимальный;)
+ @item(alpha - угол поворота поясов вокруг оси X, градусы.)
 @end(list)"
   (format t
-          "LINE: Line ~A ~A ~A ~A
+          "LINE: Line ~A ~A ~A ~A ~A
   Apply Instancing Transform = On
   Colour = 1, ~A, 0
   Colour Map = Default Colour Map
@@ -204,8 +234,8 @@ END
   Max = 0.0 [Pa]
   Min = 0.0 [Pa]
   Option = Two Points
-  Point 1 = ~A [m], ~A [m], 0 [m]
-  Point 2 = ~A [m], ~A [m], 0 [m]
+  Point 1 = ~F [m], ~F [m], ~F [m]
+  Point 2 = ~F [m], ~F [m], ~F [m]
   Range = Global
   Visibility = On
   OBJECT VIEW TRANSFORM: 
@@ -227,10 +257,21 @@ END
   END
 END
 "
-          (number-to-string x) (number-to-string y) (number-to-string r-min) (number-to-string r-max)
+          (number-to-string alpha)
+          (number-to-string x)
+          (number-to-string y)
+          (number-to-string r-min)
+          (number-to-string r-max)
+          
           (nth-value 1 (floor 1 2)) ;; color
-          (mm->m x) (mm->m (+ y r-min))
-          (mm->m x) (mm->m (+ y r-max)))
+          ;; x y z Point 1
+          (mm->m x)
+          (* (cos (math/coord:dtr alpha)) (mm->m (+ y r-min)))
+          (* (sin (math/coord:dtr alpha)) (mm->m (+ y r-min)))
+          ;; x y z Point 2
+          (mm->m x)
+          (* (cos (math/coord:dtr alpha)) (mm->m (+ y r-max)))
+          (* (sin (math/coord:dtr alpha)) (mm->m (+ y r-max))))
   
   (loop :for i :from 0 :below number :do
     (let ((theta-i    (+ theta-min (* (/ i      number) (- theta-max theta-min))))
@@ -238,7 +279,7 @@ END
 
       (format t
               "
-SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
+SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A ~A
   Apply Instancing Transform = On
   Apply Texture = Off
   Blend Texture = On
@@ -259,7 +300,7 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   Line Colour = 0, 0, 0
   Line Colour Mode = Default
   Line Width = 1
-  Location List = /LINE:Line ~A ~A ~A ~A
+  Location List = /LINE:Line ~A ~A ~A ~A ~A
   Max = 0.0 [Pa]
   Meridional Point 1 = 0 [m], 1 [m]
   Meridional Point 2 = 1 [m], 2 [m]
@@ -270,8 +311,8 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   Project to AR Plane = On
   Range = Global
   Render Edge Angle = 0 [degree]
-  Rotation Axis From = 0 [m], ~A [m], 0 [m]
-  Rotation Axis To = 1 [m], ~A [m], 0 [m]
+  Rotation Axis From = 0 [m], ~F [m], ~F [m]
+  Rotation Axis To = 1 [m], ~F [m], ~F [m]
   Rotation Axis Type = Rotation Axis
   Specular Lighting = On
   Starting Axial Shift = 0.0 [m]
@@ -311,15 +352,33 @@ SURFACE OF REVOLUTION: SURFACE ~A ~A ~A ~A ~A
   END
 END
 "
-              (number-to-string x) (number-to-string r-min) (number-to-string r-max) (number-to-string theta-i) (number-to-string theta-i+1) 
-              (nth-value 1 (floor (1+ i) 2)) ;; color
-              (number-to-string x) (number-to-string y) (number-to-string r-min) (number-to-string r-max) ;; Location
-              (mm->m y) ;; Rotation Axis From y
-              (mm->m y) ;; Rotation Axis To y
-              theta-i theta-i+1
+              (number-to-string alpha)
+              (number-to-string x)
+              (number-to-string r-min)
+              (number-to-string r-max)
+              (number-to-string theta-i)
+              (number-to-string theta-i+1)
+              ;; Color
+              (nth-value 1 (floor (1+ i) 2)) 
+              ;; Location
+              (number-to-string alpha)
+              (number-to-string x)
+              (number-to-string y)
+              (number-to-string r-min)
+              (number-to-string r-max)
+              ;; y z Rotation Axis From 
+              (* (cos (math/coord:dtr alpha)) (mm->m y))
+              (* (sin (math/coord:dtr alpha)) (mm->m y))
+              ;; y z Rotation Axis To
+              (* (cos (math/coord:dtr alpha)) (mm->m y))
+              (* (sin (math/coord:dtr alpha)) (mm->m y))
+              ;; Theta
+              theta-i
+              theta-i+1
               ))))
-
-(make-radial-belts 10 50.0 530 14.0 24.0 -180.00 180.0)
+(progn
+  (make-radial-belts 10 50.0 530 10.0 110 -180.00 180.0 0)
+  (make-radial-belts 10 50.0 530 10.0 110 -180.00 180.0 22.5))
 
 (defun mk-lines (l-prefix x-coord r-max r-min by)
   (loop :for i :from r-min :below r-max :by by :do
