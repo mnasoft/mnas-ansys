036dbab0f70d9f5a923ec58d7189ad19929d1cf5
Nick Matvyeyev <mnasoft@gmail.com>
2022-12-01 09:28:01 +0200

    1 дек 2022 г.  7:28:01

--- src/dia/dia.lisp
+++ src/dia/dia.lisp
@@ -16,6 +16,14 @@
 (defparameter *initial-directory* *default-pathname-defaults*)
 
 (defun open-dia ()
+  "@b(Описание:) функция @b(open-dia) возвращает открытый tin-файл.
+
+
+ @b(Пример использования:)
+@begin[lang=lisp](code)
+ (open-dia)
+@end(code)
+"
   (setf *tin*
         (mnas-ansys:open-tin-file
          (mnas-file-dialog:get-open-file
@@ -28,6 +36,11 @@
   "@b(Описание:) функция @b(choose-directory) запускате на выполнение
  диалог, при помощи которого можно выбрать стартовый каталог для
  диалога открытия файлов.
+
+ @b(Пример использования:)
+@begin[lang=lisp](code)
+ (choose-directory)
+@end(code)
 "
   (setf *initial-directory*
         (mnas-file-dialog:choose-directory)))
