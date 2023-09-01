3315b7d88242d5551a6fabb7d8cb964d99fb9740
Mykola Matvyeyev <mnasoft@gmail.com>
2023-09-01 19:06:17 +0300

    Fri Sep  1 16:06:16 UTC 2023

--- tcl/load_all.tcl
+++ tcl/load_all.tcl
@@ -1,4 +1,5 @@
 # source d:/home/_namatv/PRG/msys64/home/namatv/quicklisp/local-projects/ANSYS/mnas-ansys/tcl/load_all.tcl
+# source d:/PRG/msys64/home/mnaso/quicklisp/local-projects/ANSYS/mnas-ansys/tcl/load_all.tcl
 
 proc load_all {} {
     set directiry d:/home/_namatv/PRG/msys64/home/namatv/quicklisp/local-projects/ANSYS/mnas-ansys/tcl/ic/
