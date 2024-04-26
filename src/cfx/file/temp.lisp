9f00797942c190df01091b0911731c4aa704003b
Mykola Matvyeyev <mnasoft@gmail.com>
2024-04-26 18:18:56 +0300

    Fri Apr 26 15:18:56 UTC 2024

--- src/cfx/file/temp.lisp
+++ src/cfx/file/temp.lisp
@@ -35,8 +35,9 @@
   (save        *res*)          ; Сохраняем объект в res-файл
   )
 
+;;; Загружаем данные из файла
 #+nil
-(setf *res* (load-instance *res*)) ; Загружаем данные из файла
+(setf *res* (load-instance *res*))
 
 (iterations *res*) ; Количество итераций 
 (iteration-start *res*) ; Начальная итерация
@@ -55,14 +56,26 @@
  (<res>-ccl *res*) t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;
+;;;; t-03 t-02 dt
 
+(defmethod t-03 ((res <res>))
+  (- (math/stat:average-value
+      (coerce 
+       (mnas-ansys/cfx/file/mon:<mon>-data
+        (gethash "T03:" (<res>-mon res)))
+       'list))
+     273.15))
 
-(math/stat:average-value
- (coerce 
-  (mnas-ansys/cfx/file/mon:<mon>-data
-   (gethash "GT OUT 109 100:Total Temperature" (<res>-mon *res*)))
-  'list))
+(defmethod t-02 ((res <res>))
+  (mnas-ansys/ccl:find-in-tree-in-deep 
+   '(("FLOW" "Flow Analysis 1") ("BOUNDARY" "INLET") "Total Temperature")
+   (<res>-ccl *res*) t))
+
+(defmethod dt ((res <res>))
+  (- (t-03 res) (t-02 res)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; aver-temp max-temp
 
 (defmethod aver-temp ((res <res>)
                       &key 
@@ -70,12 +83,8 @@
                         (h-i-to   109)
                         (w-i-from 100)
                         (w-i-to   134)
-                        (h-rel
-                            (loop :for h-i :from h-i-from :to h-i-to
-                                  :for h-r :from 0.05 :by 0.1
-                                  :collect h-r)
-                                  )
-                        )
+                        (h-rel (loop :for h-i :from h-i-from :to h-i-to
+                                     :for h-r :from 0.05 :by 0.1 :collect h-r)))
   (let ((t-mid
           (loop :for h :from h-i-from :to h-i-to
                 :for h-r :in h-rel
@@ -94,20 +103,24 @@
                              (<res>-mon res)))
                            'list))))
                   273.15)))))
-    (vgplot:plot (mapcar #'second t-mid) (mapcar #'first t-mid))))
+    t-mid))
+
+#+nil
+(progn
+  (vgplot:title  "Радиальная эпюра температур")
+  (vgplot:xlabel "T, C")
+  (vgplot:ylabel "h" )
+  (vgplot:axis '(1100 1400 0 1))
+  (vgplot:plot (mapcar #'second t-mid) (mapcar #'first t-mid)))
 
 (defmethod max-temp ((res <res>)
-                      &key 
-                        (h-i-from 100)
-                        (h-i-to   109)
-                        (w-i-from 100)
-                        (w-i-to   134)
-                        (h-rel
-                            (loop :for h-i :from h-i-from :to h-i-to
-                                  :for h-r :from 0.05 :by 0.1
-                                  :collect h-r)
-                                  )
-                        )
+                     &key 
+                       (h-i-from 100)
+                       (h-i-to   109)
+                       (w-i-from 100)
+                       (w-i-to   134)
+                       (h-rel (loop :for h-i :from h-i-from :to h-i-to
+                                    :for h-r :from 0.05 :by 0.1 :collect h-r)))
   (let ((t-max
           (loop :for h :from h-i-from :to h-i-to
                 :for h-r :in h-rel
@@ -126,47 +139,97 @@
                              (<res>-mon res)))
                            'list))))
                   273.15)))))
-    (vgplot:plot (mapcar #'second t-max) (mapcar #'first t-max))))
-
-(defmethod t-03-00 ((res <res>))
-  (- (math/stat:average-value
-      (coerce 
-       (mnas-ansys/cfx/file/mon:<mon>-data
-        (gethash "T03:" (<res>-mon res)))
-       'list))
-     273.15))
-
-(defmethod t-02 ((res <res>))
-  (mnas-ansys/ccl:find-in-tree-in-deep 
-   '(("FLOW" "Flow Analysis 1") ("BOUNDARY" "INLET") "Total Temperature")
-   (<res>-ccl *res*) t))
-
-(defmethod dt ((res <res>))
-  (- (t-03-00 res) (t-02 res)))
-
-(t-03-00 *res*)
-
-(dt *res*)
-
-(aver-temp *res*)
-
-(max-temp *res*)
-
-(progn
-  (setf *res* (load-instance *res*)) ; Загружаем объект из s-obj-файла
-
-  (<res>-data *res*)
-  (mon-select ".*" *res*)
-
+    t-max ))
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; aver-teta max-teta
 
-(<res>-mon *res*)
+(defmethod aver-teta ((res <res>)
+                      &key 
+                        (h-i-from 100)
+                        (h-i-to   109)
+                        (w-i-from 100)
+                        (w-i-to   134)
+                        (h-rel (loop :for h-i :from h-i-from :to h-i-to
+                                     :for h-r :from 0.05 :by 0.1 :collect h-r)))
+  (let* ((t-mid
+           (aver-temp res :h-i-from h-i-from
+                          :h-i-to h-i-to
+                          :w-i-from w-i-from
+                          :w-i-to w-i-to
+                          :h-rel h-rel ))
+         (t-02 (t-02 res))
+         (dt   (dt res))
+         (teta-mid (mapcar #'(lambda (x)
+                               (list (first x)  (/ (- (second x) t-02) dt)))
+                           t-mid)))
+    teta-mid))
 
-(<res>-ccl *res*)
+(defmethod max-teta ((res <res>)
+                     &key 
+                       (h-i-from 100)
+                       (h-i-to   109)
+                       (w-i-from 100)
+                       (w-i-to   134)
+                       (h-rel (loop :for h-i :from h-i-from :to h-i-to
+                                    :for h-r :from 0.05 :by 0.1 :collect h-r)))
+  (let* ((t-max
+           (max-temp res :h-i-from h-i-from
+                         :h-i-to h-i-to
+                         :w-i-from w-i-from
+                         :w-i-to w-i-to
+                         :h-rel h-rel ))
+         (t-02 (t-02 res))
+         (dt   (dt res))
+         (teta-max (mapcar #'(lambda (x)
+                               (list (first x)  (/ (- (second x) t-02) dt)))
+                           t-max)))
+    teta-max))
 
+(defmethod teta ((res <res>)
+                 &key 
+                   (h-i-from 100)
+                   (h-i-to   109)
+                   (w-i-from 100)
+                   (w-i-to   134)
+                   (h-rel (loop :for h-i :from h-i-from :to h-i-to
+                                :for h-r :from 0.05 :by 0.1 :collect h-r)))
+  (let ((aver-teta
+          (aver-teta res :h-i-from h-i-from
+                         :h-i-to h-i-to
+                         :w-i-from w-i-from
+                         :w-i-to w-i-to
+                         :h-rel h-rel ))
+        (max-teta
+          (max-teta res :h-i-from h-i-from
+                        :h-i-to h-i-to
+                        :w-i-from w-i-from
+                        :w-i-to w-i-to
+                        :h-rel h-rel )))
+    (let ((name
+            (format nil "~A[~A]"
+                    (ppcre:regex-replace-all "_"  (pathname-name (<res>-pathname res)) "-")
+                    (iterations *res*)
+                    )))
+      (progn
+        (vgplot:title  "Радиальная эпюра относительных температур")
+    
+        (vgplot:xlabel "T, C")
+        (vgplot:ylabel "h" )
+        (vgplot:axis '(0.8 1.3 0 1))
+        (vgplot:plot (mapcar #'second max-teta) (mapcar #'first max-teta)
+                     (concatenate 'string name ":" "U_{ max}")
+                     (mapcar #'second aver-teta) (mapcar #'first aver-teta)
+                     (concatenate 'string name ":" "U_{ mid}"))
+        (vgplot:text-show-label)))))
+    
+(teta *res*)
 
+(max-temp *res*)
+(max-teta *res*)
+(aver-teta *res*)
+(aver-temp *res*)
 
-(t-03-00 *res*)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
