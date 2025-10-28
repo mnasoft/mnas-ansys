5f66acc0b5d1af683f777fcda7ff48aa73697466
Mykola Matvyeyev <mnasoft@gmail.com>
2025-10-28 12:42:01 +0200

    2025-10-28 10:42:01 UTC

---
+++ src/cfx/pre/nodgui/test3.lisp
@@ -0,0 +1,47 @@
+(ql:quickload '(:sdl2 :cl-opengl :cl-glut :cl-glu))
+
+(defun run-demo ()
+  (sdl2:with-init (:everything)
+    (sdl2:with-window (win :title "SDL2 + GLUT Teapot"
+                           :w 800 :h 600
+                           :flags '(:opengl :shown))
+      (sdl2:with-gl-context (gl win)
+        ;; Настройка OpenGL
+        (gl:viewport 0 0 800 600)
+        (gl:matrix-mode :projection)
+        (gl:load-identity)
+        (glu:perspective 45.0 (/ 800.0 600.0) 1.0 100.0)
+        (gl:matrix-mode :modelview)
+        (glut:init) 
+
+        ;; Цикл событий
+        (sdl2:with-event-loop (:method :poll)
+          (:quit () t)
+          (:idle ()
+                 (gl:clear :color-buffer-bit :depth-buffer-bit)
+                 (gl:load-identity)
+                 (gl:translate 0.0 0.0 -8.0)
+                 (gl:enable :depth-test)
+                 (gl:enable :lighting)
+                 (gl:enable :light0)
+                 
+                 (gl:light :light0 :position '(0 1 1 0))
+                 (gl:light :light0 :diffuse '(0.9 1.0 1.0 0))
+
+                 (gl:material :front :ambient '(0.2 0.2 0.2 1.0))
+                 (gl:material :front :diffuse '(0.2 0.6 1.0 1.0))
+                 (gl:material :front :specular '(1.0 1.0 1.0 1.0))
+                 (gl:material :front :shininess 50.0)
+                 (gl:rotate -90 1 0 0) 
+                 
+                 (gl:rotate (mod (* (get-internal-real-time)
+                                    (/ (* 1/60 360.0) internal-time-units-per-second))
+                                 360.0)
+                            0 1 0) 
+                 (gl:color 1.0 0.2 2.0)
+                 ;; Рисуем чайник GLUT
+                 (glut:solid-teapot 1.0)
+
+                 (sdl2:gl-swap-window win)))))))
+
+(run-demo)
